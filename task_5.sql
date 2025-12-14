8f503e763423d7340f1696511002b754cd984c66
Ogochukwu Ike <ogochike508@gmail.com>
2025-12-14 22:36:34 +0100

    jdld

---
+++ task_5.sql
@@ -0,0 +1,14 @@
+USE alx_book_store;
+
+INSERT INTO Customers (
+    customer_id,
+    customer_name,
+    email,
+    address
+)
+VALUES (
+    1,
+    'Cole Baidoo',
+    'cbaidoo@sandtech.com',
+    '123 Happiness Ave.'
+);
