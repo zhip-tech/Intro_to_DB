d7d4c1bfec5206e89f49e10ac50abbf3666b2b79
Ogochukwu Ike <ogochike508@gmail.com>
2025-12-14 22:20:09 +0100

    gole

---
+++  MySQLServer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""   # change if your MySQL has a password
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error while connecting to MySQL: {e}")
+
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        cursor.close()
+        connection.close()
